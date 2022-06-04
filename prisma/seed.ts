1eb3b6854064460abd6279bbeeff1bb9c35073b7
yescine <bouchoucha.yassine@gmail.com>
2022-06-04 17:41:59 +0100

    seeding db

---
+++ prisma/seed.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "prisma/prisma-client";
+import { links } from "../data/links";
+const prisma = new PrismaClient();
+
+export async function main() {
+  await prisma.user.create({ data: { email: "dev@dev.com", role: "ADMIN" } });
+
+  await prisma.link.createMany({ data: links });
+}
+
+main()
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
