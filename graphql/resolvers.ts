2d10097f3bfab2ae23db77730e2c93b2aa0a74bb
yescine <bouchoucha.yassine@gmail.com>
2022-06-04 18:30:18 +0100

    add context and dynamic resolver

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,14 +1,8 @@
+import { prisma } from "lib/prisma";
 export const resolvers = {
   Query: {
-    links: () => [
-      {
-        category: "Open Source",
-        description: "Fullstack React framework",
-        id: "8a9020b2-363b-4a4f-ad26-d6d55b51bqes",
-        imageUrl: "https://nextjs.org/static/twitter-cards/home.jpg",
-        title: "Next.js",
-        url: "https://nextjs.org",
-      },
-    ],
+    links: async (_parent,_args,ctx) => {
+      return await prisma.link.findMany();
+    },
   },
 };
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -3,15 +3,16 @@ import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "../../graphql/schema";
 import { resolvers } from "../../graphql/resolvers";
 import Cors from "micro-cors";
+import { createContext } from "graphql/context";
 
 const cors = Cors();
-const appoloServer = new ApolloServer({ typeDefs, resolvers });
+const appoloServer = new ApolloServer({ typeDefs, resolvers, context: createContext });
 const startServer = appoloServer.start();
 
 export default cors(async (req: NextApiRequest, res: NextApiResponse) => {
-  if(req.method==="OPTIONS"){
-    res.end()
-    return false
+  if (req.method === "OPTIONS") {
+    res.end();
+    return false;
   }
   await startServer;
   await appoloServer.createHandler({
